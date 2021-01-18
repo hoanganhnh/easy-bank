0c263587809ae2dabe06acf55f8af0b743c1c189
anhnh <hoahoanganh20012001@gmail.com>
2021-01-18 18:51:50 +0700

    Nop bai lan 2

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click',function() {
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')) {// close hamburger menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');    
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }else {// open hamburger menu
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
